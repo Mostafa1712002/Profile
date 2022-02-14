a7571cf336d22c90f65fc02c60697e50ca4761aa
Mostafa1712002 <ebrahimmostafa@gmail.com>
2022-02-14 12:29:38 +0200

    init

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Ibn AlDAMRDASH</title>
+  <!-- <base href="./"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="assets/images/logo.png">
+  <meta name="description" content="hi there i'm php developer ">
+  <meta name="keywords" content="
+  ibnalamrdash,mostafaebrahim,MOSTAFAEBRAHIM,mostafa-ebrahim,laravel-developer,php-developer,web-developer,wordpress-developer,wordpress-developer,software-engineer,
+  
+  ">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+  <div class="home  d-flex justify-content-center align-items-center">
+    <div class="container py-5 ">
+      <!-- Start of Website Intro -->
+      <div class="intro">
+        <h2 class="pb-4">
+          <span>Hello World! </span>
+          <img src="assets/images/waving-hand.png" class="wavingHand">
+        </h2>
+        <div class="intro-info">
+          <p>I'm Happy to be here, I'm Eng/Mostafa Ebrahim SoftWare Engineering , PHP Developer (Laravel framework)</p>
+          <div>
+            <h6>Get in touch:</h6>
+            <ul class="contactsList d-flex justify-content-center ps-0">
+              <li>
+                <a href="https://api.whatsapp.com/send?phone=201022348224" target="_blank"><i class="fab fa-whatsapp"></i></a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/mostafa-ibrahim-485492213" target="_blank"><i class="fab fa-linkedin"></i></a>
+              </li>
+              <li>
+                <a href="https://www.github.com/Mostafa1712002" target="_blank"><i class="fab fa-github"></i></a>
+              </li>
+              <li>
+                <a href="mailto:ossamaabdein@gmail.com"><i class="fas fa-envelope-square pe-2"></i></a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <a href="assets/php-cv.pdf" download> My Cv</a>
+
+      </div>
+
+    </div>
+  </div>
+
+
+  <!-- footer -->
+  <div class="footer pt-4" id="test">
+    <div class="contacts d-flex justify-content-center align-items-center flex-column">
+      <ul class="contactsList d-flex mb-2">
+        <li>
+          <a href="https://api.whatsapp.com/send?phone=201022348224" target="_blank"><i class="fab fa-whatsapp"></i></a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/mostafa-ibrahim-485492213/" target="_blank"><i class="fab fa-linkedin"></i></a>
+        </li>
+        <li>
+          <a href="https://www.github.com/Mostafa1712002" target="_blank"><i class="fab fa-github"></i></a>
+        </li>
+        <li>
+          <a href="mailto:mostafaibrahim1712002@gmail.com"><i class="fas fa-envelope-square pe-2"></i></a>
+        </li>
+      </ul>
+      <div class="credits">
+        <span>@<?php echo date("Y") ?> ibn AlDamrdash. All Rights Reserved.</span>
+      </div>
+    </div>
+  </div>
+
+  <!-- / footer -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
